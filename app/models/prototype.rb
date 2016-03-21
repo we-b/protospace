4559ee4bc3ee6bacf391349099d384b4ad61569f
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-03-21 20:04:31 +0900

    add validates and association

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,8 @@
 class Prototype < ActiveRecord::Base
+  has_many :captured_images, dependent: :delete_all
+
+  validates: :title,
+             :catch_copy,
+             :concept,
+             presece: true
 end
