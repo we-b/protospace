0d5601df54820fbbc1dddecc209ea848f9955d2b
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-03-21 20:02:46 +0900

    creat prototypes table

--- db/migrate/20160321105022_create_prototypes.rb
+++ db/migrate/20160321105022_create_prototypes.rb
@@ -1,7 +1,9 @@
 class CreatePrototypes < ActiveRecord::Migration
   def change
     create_table :prototypes do |t|
-
+      t.string :title
+      t.string :catch_copy
+      t.text   :concept
       t.timestamps null: false
     end
   end
