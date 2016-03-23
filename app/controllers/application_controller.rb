0303d54a53a642516b5857b1e68a64f260fc5156
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-03-23 22:33:03 +0900

    set strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(:name, :profile, :position, :occupation, :avatar)
+    devise_parameter_sanitizer.for(:account_update).push(:name, :profile, :position, :occupation, :avatar)
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
