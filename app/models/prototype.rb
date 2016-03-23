4494763cc9406a5c423d35edc3c32ea95968ffbe
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-03-23 18:49:27 +0900

    delete model and migration file

--- app/models/prototype.rb
+++
@@ -1,8 +0,0 @@
-class Prototype < ActiveRecord::Base
-  has_many :captured_images, dependent: :destory
-
-  validates: :title,
-             :catch_copy,
-             :concept,
-             presece: true
-end
