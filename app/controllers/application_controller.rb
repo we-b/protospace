c7c4058425afce5a2902a2f4db1a43b5caa9392d
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-03-23 22:04:00 +0900

    be going to define next pull request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:name, :profile, :position, :occupation, :avatar)
-    devise_parameter_sanitizer.for(:account_update).push(:name, :profile, :position, :occupation, :avatar)
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-   validates :name, presence: true
+  validates :name, presence: true
 end
