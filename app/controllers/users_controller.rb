48722ae7644b0be31d4d5e1af042a7c701b2b7c2
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-04 19:33:36 +0900

    define edit and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to :root, notice: 'The user information was successfully updated'
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :position,
+      :profile,
+      :occupation,
+      :avatar
+    )
+  end
 end
