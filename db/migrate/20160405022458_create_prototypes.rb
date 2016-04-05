a457e318f98cdbea582d06dd4b02f83548f894d5
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-05 23:36:12 +0900

    add column to prototypes table

--- db/migrate/20160405022458_create_prototypes.rb
+++ db/migrate/20160405022458_create_prototypes.rb
@@ -1,6 +1,10 @@
 class CreatePrototypes < ActiveRecord::Migration
   def change
     create_table :prototypes do |t|
+      t.string :title
+      t.string :catch_copy
+      t.text   :concept
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
