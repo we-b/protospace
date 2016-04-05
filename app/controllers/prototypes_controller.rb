59239e59f737f04d5a504f4983cfb16f0ff6d17c
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-06 00:21:06 +0900

    define new and create action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,34 @@
 class PrototypesController < ApplicationController
+
   def index
   end
 
   def new
+    @prototype = Prototype.new
+    @prototype.captured_images.build
+  end
+
+  def create
+    prototype = Prototype.new(prototype_params)
+    if prototype.save
+      redirect_to :root
+    else
+      render action: :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(
+      :title,
+      :catch_copy,
+      :concept,
+      :user_id,
+      captured_images_attributes: [:content, :status]
+    )
   end
 end
