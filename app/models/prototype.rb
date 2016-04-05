d27094cd6a6d7bc857dff3d37809797eba9067bf
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-05 23:32:49 +0900

    add prototype association and validation

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,11 @@
 class Prototype < ActiveRecord::Base
+  belongs_to :user
+  has_many :caputured_images, dependent: :destroy
+
+  accepts_nested_attributes_for :captured_images
+
+  validates :titie,
+            :catch_copy,
+            :concept,
+            presence: true
 end
