b5baea24284e6be5c3d7a0cded56d510122f73cd
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-08 01:18:42 +0900

    define routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :show, :new, :create, :destroy]
+  resources :prototypes
   resources :users, only: [:show, :edit, :update]
 end
