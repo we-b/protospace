e24d83d9dbcc9424046a28e18922cd110c273b1d
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-08 21:17:40 +0900

    delete unnecessary methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  include ActionView::Helpers
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -9,8 +7,4 @@ class User < ActiveRecord::Base
   has_many :prototypes
 
   validates :name, presence: true
-
-  def set_profile_photo
-    avatar.presence || asset_path('assets/noimage.png')
-  end
 end
