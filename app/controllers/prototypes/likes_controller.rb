000c0e4b3a498def699ce0f1693a1ff10a9465da
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-09 02:45:33 +0900

    re-create likes controller

---
+++ app/controllers/prototypes/likes_controller.rb
@@ -0,0 +1,18 @@
+class Prototypes::LikesController < ApplicationController
+  before_action :set_prototype, only: :create
+
+  def create
+    @prototype.likes.create(params.permit(:user_id))
+  end
+
+  def destroy
+    Like.find(params[:id]).destroy
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
+   private
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+end
