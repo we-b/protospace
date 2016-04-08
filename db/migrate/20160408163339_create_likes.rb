177a7dc3b17e2cb120bbce2b3d4c2cb1b0b33f4d
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-09 02:46:26 +0900

    change schema

--- db/migrate/20160408163339_create_likes.rb
+++ db/migrate/20160408163339_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration
   def change
     create_table :likes do |t|
-
-      t.timestamps null: false
+      t.references :prototype, foreign_key: true
+      t.references :user, foreign_key: true
     end
   end
 end
