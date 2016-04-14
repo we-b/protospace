a6fea12811d0c5e8d211aa1561f4669deb792e9c
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-15 02:06:46 +0900

    set prototype for newest and popular

--- app/controllers/prototypes/newest_controller.rb
+++ app/controllers/prototypes/newest_controller.rb
@@ -1,2 +1,7 @@
 class Prototypes::NewestController < ApplicationController
+  def index
+    @prototypes = Prototype.eager_load(:user, :main_image).order(created_at: :DESC)
+    @type = 'newest'
+    render template: 'prototypes/index'
+  end
 end
--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -1,4 +1,7 @@
 class Prototypes::PopularController < ApplicationController
   def index
+    @prototypes = Prototype.eager_load(:user, :main_image).order(likes_count: :DESC)
+    @type = 'popular'
+    render template: 'prototypes/index'
   end
 end
