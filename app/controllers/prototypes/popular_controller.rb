dab4477f54dde12d4e4bf5b1010f6ba04f18b1b0
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-17 14:46:42 +0900

    create newest and popular index view

--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -2,6 +2,5 @@ class Prototypes::PopularController < ApplicationController
   def index
     @prototypes = Prototype.eager_load(:user, :main_image).order(likes_count: :DESC)
     @type = 'popular'
-    render template: 'prototypes/index'
   end
 end
