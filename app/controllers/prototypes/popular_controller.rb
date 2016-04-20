11eb27f62dcb0f9c17b36a078a7486410b8fd832
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-20 21:22:04 +0900

    add page methods

--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -1,6 +1,8 @@
 class Prototypes::PopularController < ApplicationController
   def index
-    @prototypes = Prototype.eager_load(:user, :main_image).order(likes_count: :DESC)
+    @prototypes = Prototype.eager_load(:user, :main_image)
+                           .order(likes_count: :DESC)
+                           .page(params[:page])
     @type = 'popular'
   end
 end
