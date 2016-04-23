0ed593e90adbca5d05974d0f08a33001d0a40efb
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-23 16:34:45 +0900

    setting keys for connecting s3 and change packet based on env

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,18 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Settings.aws.access_key,
+    aws_secret_access_key: Settings.aws.secret_access_key,
+    region: Settings.aws.s3_bucket_region,
+  }
+
+  case Rails.env
+    when 'development'
+     config.fog_directory = Settings.aws.s3_bucket_name_dev
+     config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/#{config.fog_directory}"
+
+    else
+     config.fog_directory = 'test.dummy'
+     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/test.dummy'
+  end
+end
