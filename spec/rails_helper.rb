e9d2e6e00c781557988cd0ffb231c73cadcdb1cc
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-04-28 01:48:37 +0900

    Added a code in rails_helper to abbreviate writing FactoryGirl in spec files.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
