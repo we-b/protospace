5478633d422afce56cb9d87c456c0a9aa04339ba
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-04-28 02:13:26 +0900

    Test code when there is no name.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,12 @@ describe User do
       expect(build(:user)).to be_valid
     end
 
+    it "is invalid without a name" do
+      user = build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
   end
 end
 
