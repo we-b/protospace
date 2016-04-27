14bb3ccd08f3711db1f0de5ed9c7f63327e6958a
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-04-28 02:14:54 +0900

    Test code when there is no email.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,12 @@ describe User do
       expect(user.errors[:name]).to include("can't be blank")
     end
 
+    it "is invalid without an email" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
   end
 end
 
