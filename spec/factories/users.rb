f1a2e007f6fa4e6465ced1e3046ae7e0b7968fac
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-04-29 15:04:14 +0900

    Added password_confirmation to a dummy user.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 FactoryGirl.define do
   factory :user do
+    password = Faker::Internet.password(8)
     name { Faker::Name.name }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(8) }
+    password password
+    password_confirmation password
     profile { Faker::Hacker.say_something_smart }
     position { Faker::Company.profession }
     occupation { Faker::Company.catch_phrase }
