0835d795d1390f7b3836f4b8fb41ccbb30142b9b
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-04-29 20:19:40 +0900

    write setting for polteregist

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'capybara/poltergeist'
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -11,4 +12,5 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+  Capybara.javascript_driver = :poltergeist
 end
