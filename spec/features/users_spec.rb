043fd74c9839ff39f9dc741e44e0859b7410d612
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-05-05 02:37:27 +0900

    make visible true for file field

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature 'Sign in', type: :feature, js: true do
+RSpec.feature 'Sign in', type: :feature do 
   given(:user) { build(:user) }
   given(:login_user) { create(:user) }
   given(:prototype) { build(:prototype, :with_sub_images) }
 
-  scenario 'Create new user and Sign out' do
+  scenario 'Create new user and Sign out', js: true do
     def sign_up(user)
       # Fill in the blank
       fill_in 'user_name', with: user.name
@@ -28,17 +28,15 @@ RSpec.feature 'Sign in', type: :feature, js: true do
     # Check flash message to show sign in successfully
     expect(page).to have_selector '.alert-success', text: 'Welcome! You have signed up successfully.'
     # Click dropdown toggle
-    page.find("#js-dropdown").click
-    page.save_screenshot('screenshot.png')
-    # wait for applying js
-    sleep 0.5
+    click_on user.name
     # Click Logout button
+    page.evaluate_script('$(".dropdown").addClass("open")')
     click_on 'Logout'
     # Check flash message to show sign out successfully
     expect(page).to have_selector '.alert-success', text: 'Signed out successfully.'
   end
 
-  scenario 'Login and Post prototype' do
+  scenario 'Login and Post prototype', js: true do
     def sign_in(user)
       # Login
       fill_in 'inputEmail', with: login_user.email
@@ -53,9 +51,9 @@ RSpec.feature 'Sign in', type: :feature, js: true do
       fill_in 'prototype_title', with: prototype.title
       fill_in 'prototype_catch_copy', with: prototype.catch_copy
       fill_in 'prototype_concept', with: prototype.concept
-      attach_file 'prototype[captured_images_attributes][0][content]', File.join(Rails.root, '/spec/factories/img/sample.png')
+      attach_file 'prototype[captured_images_attributes][0][content]', File.join(Rails.root, '/spec/factories/img/sample.png'),  visible: false
       1.upto(3) do |i|
-        attach_file "prototype[captured_images_attributes][#{i}][content]", File.join(Rails.root, '/spec/factories/img/sample.png')
+        attach_file "prototype[captured_images_attributes][#{i}][content]", File.join(Rails.root, '/spec/factories/img/sample.png'), visible: false
       end
       click_on 'PUBLISH'
     end
@@ -69,7 +67,6 @@ RSpec.feature 'Sign in', type: :feature, js: true do
     expect(page).to have_selector '.alert-success', text: 'Signed in successfully.'
     # Click New photo button
     click_on 'New Photo'
-    page.save_screenshot('screenshotProto.png')
     # Post prototype
     post_prototype_by(user)
     # Check flash message to create prototype successfully
