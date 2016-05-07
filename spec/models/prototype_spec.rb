df561a96ea66a9dc3848a573faffe0c2bbdc320c
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-05-07 15:43:25 +0900

    Test code when there is no concept.

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -20,6 +20,12 @@ describe Prototype do
         prototype.valid?
         expect(prototype.errors[:catch_copy]).to include("can't be blank")
       end
+
+      it "is invalid without a concept" do
+        prototype = build(:prototype, concept: nil)
+        prototype.valid?
+        expect(prototype.errors[:concept]).to include("can't be blank")
+      end
     end
   end
 end
