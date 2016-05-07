df0f0e0886f365a6b797ec3c45002356420db1e9
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-05-07 15:36:13 +0900

    Test code when there is every information present.

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -1,1 +1,11 @@
 require 'rails_helper'
+
+describe Prototype do
+  describe '#create' do
+    context "with valid attributes" do
+      it "is valid with title, catch_copy and concept" do
+        expect(build(:prototype)).to be_valid
+      end
+    end
+  end
+end
