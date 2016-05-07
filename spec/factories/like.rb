7583e7a44920fbad97504db549ccff8209a43816
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-05-07 23:57:39 +0900

    Created a factory inheriting "like".

--- spec/factories/like.rb
+++ spec/factories/like.rb
@@ -2,5 +2,10 @@ FactoryGirl.define do
   factory :like, class: Like do
     prototype_id  { Faker::Number.number(2) }
     user_id       { Faker::Number.number(2) }
+
+    factory :like_with_prototype_id_and_user_id do
+      sequence(:prototype_id) { |n| n }
+      sequence(:user_id) { |n| n }
+    end
   end
 end
