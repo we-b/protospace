a7c793c71d4d28b49523ba4e5b4fe4e6a8b955f2
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-05-09 02:02:24 +0900

    Modified the name of each test.

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -3,25 +3,25 @@ require 'rails_helper'
 describe Prototype do
   describe '#create' do
     context "with valid attributes" do
-      it "is valid with title, catch_copy and concept" do
+      it "has a valid factory" do
         expect(build(:prototype)).to be_valid
       end
     end
 
     context "without valid attributes" do
-      it "is invalid without a title" do
+      it "is missing a title" do
         prototype = build(:prototype, title: nil)
         prototype.valid?
         expect(prototype.errors[:title]).to include("can't be blank")
       end
 
-      it "is invalid without a catch_copy" do
+      it "is missing a catch_copy" do
         prototype = build(:prototype, catch_copy: nil)
         prototype.valid?
         expect(prototype.errors[:catch_copy]).to include("can't be blank")
       end
 
-      it "is invalid without a concept" do
+      it "is missing a concept" do
         prototype = build(:prototype, concept: nil)
         prototype.valid?
         expect(prototype.errors[:concept]).to include("can't be blank")
