3a8333138c84b9277d57b27d18cb92a311662d19
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-05-08 22:51:16 +0900

    Modified the name of each test.

--- spec/models/captured_image_spec.rb
+++ spec/models/captured_image_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 describe CapturedImage do
   describe '#create' do
     context "with valid attributes" do
-      it "is valid with content and status" do
+      it "has a valid factory" do
         captured_image = build(:captured_image, :main)
         expect(captured_image).to be_valid
       end
     end
 
     context "without valid attributes" do
-      it "is invalid without a content" do
+      it "is missing a content" do
         captured_image = build(:captured_image, :main, content: nil)
         captured_image.valid?
         expect(captured_image.errors[:content]).to include("can't be blank")
       end
 
-      it "is invalid without a status" do
+      it "is missing a status" do
         captured_image = build(:captured_image)
         captured_image.valid?
         expect(captured_image.errors[:status]).to include("can't be blank")
