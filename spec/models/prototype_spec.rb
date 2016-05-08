9c3bc0d1a029ab71dd53c3f00148d9691fcaca8f
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-05-09 03:26:09 +0900

    Refactored rest code for "liked_by?(user)" method.

--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -30,28 +30,29 @@ describe Prototype do
   end
 
   describe "#posted_date" do
-    context "with a matching date" do
-      it "returns May 07 Sat" do
-        prototype = build(:prototype)
-        expect(prototype.posted_date).to eq "May 07 Sat"
-      end
-    end
-
-    context "without a matching date" do
-      it "doesn't returns a matching posted_date" do
-        prototype = build(:prototype)
-        expect(prototype.posted_date).not_to eq prototype.created_at
-      end
+    it "returns May 07 Sat" do
+      prototype = build(:prototype)
+      expect(prototype.posted_date).to eq "May 07 Sat"
     end
   end
 
   describe "#liked_by?(user)" do
-    context "with a liking user" do
+    context "when liked by a user" do
       it "returns a liking record" do
-        user = create(:user_with_id)
-        prototype = create(:prototype_with_id_and_user_id)
-        like = create(:like_with_prototype_id_and_user_id)
-        expect(prototype.liked_by?(user)).to eq prototype.likes.find_by(user_id: user)
+        user = create(:user)
+        prototype = create(:prototype, user: user)
+        like = create(:like, prototype: prototype, user: user)
+        expect(prototype.liked_by?(user)).to be_truthy
+      end
+    end
+
+    context "when not liked by a user" do
+      it "returns nil" do
+        user = create(:user)
+        another_user = create(:user, id: user.id + 1)
+        prototype = create(:prototype, user: another_user)
+        like = create(:like, prototype: prototype, user: another_user)
+        expect(prototype.liked_by?(user)).to be_falsey
       end
     end
   end
