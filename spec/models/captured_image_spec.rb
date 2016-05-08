c19088bd5f27a5b25d254689341d832b138d68b9
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2016-05-08 12:03:34 +0900

    Test code when content is blank.

--- spec/models/captured_image_spec.rb
+++ spec/models/captured_image_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+describe CapturedImage do
+  describe '#create' do
+    context "without valid attributes" do
+      it "is invalid without a content" do
+        captured_image = build(:captured_image, :main, content: nil)
+        captured_image.valid?
+        expect(captured_image.errors[:content]).to include("can't be blank")
+      end
+    end
+  end
+end
