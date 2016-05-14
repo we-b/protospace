562dafeaddd40e5b054e83205551bca9dc144bf0
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-05-14 23:01:06 +0900

    test edit action

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 describe UsersController do
   before { login_user }
+
+  describe 'GET #edit' do
+    let(:user) { create(:user) }
+    it 'assigns the requested user to @user' do
+      get :edit, id: user
+      expect(assigns(:user)).to eq user
+    end
+
+    it 'renders the :edit template' do
+      get :edit, id: user
+      expect(response).to render_template :edit
+    end
+  end
 end
