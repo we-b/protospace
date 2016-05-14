863ce10881a64b612fd54b66a006428c47e6d389
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-05-14 23:06:20 +0900

    test show action

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,8 +3,21 @@ require 'rails_helper'
 describe UsersController do
   before { login_user }
 
+  let(:user) { create(:user) }
+
+  describe 'GET #show' do
+    it 'assigns the requested to @user' do
+      get :show, id: user 
+      expect(assigns(:user)).to eq user
+    end
+
+    it 'renders the :show templates' do
+      get :show, id: user
+      expect(response).to render_template :show
+    end
+  end
+
   describe 'GET #edit' do
-    let(:user) { create(:user) }
     it 'assigns the requested user to @user' do
       get :edit, id: user
       expect(assigns(:user)).to eq user
