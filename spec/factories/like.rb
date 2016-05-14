6b7e33619ea5269ad5c9de4f68d9200eb285587c
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-05-14 20:14:53 +0900

    delete emis-named model

--- spec/factories/like.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :like, class: Like do
-    association :prototype, :user
-    prototype_id  { Faker::Number.number(2) }
-    user_id       { Faker::Number.number(2) }
-  end
-end
