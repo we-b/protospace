dabbe868bde41ae4768e69cc984f234658f2d699
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-05-15 14:58:19 +0900

    check redirect to sign in page without user account

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
-describe PrototypeController do
-  
+describe PrototypesController do
+  context 'with user login' do
+    before { login_user }
+    let(:user) { create(:user) }
+  end
+
+  context 'without user login' do
+    let(:user) { create(:user) }
+    let(:params) {{
+      id: user.id,
+      user: {
+        name: user.name,
+        email: user.email,
+        position: user.position,
+        profile: user.profile,
+        occupation: user.occupation
+      }
+    }}
+    describe 'GET #new' do
+      it 'redirects sign_in page' do
+        get :new
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe 'post #create' do
+      it 'redirects sign_in page' do
+        post :create 
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'redirects sign_in page' do
+        get :edit, id: user.id
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe 'PATCH #update' do
+      it 'redirects sign_in page' do
+        patch :update, params
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'redirects sign_in page' do
+        delete :destroy, id: user.id
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+  end
 end
