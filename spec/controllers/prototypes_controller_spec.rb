b87a532d9d4d0ce1a03eed3578299e2f2ce068b0
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2016-05-16 21:15:55 +0900

    test edit action

--- spec/controllers/prototypes_controller_spec.rb
+++ spec/controllers/prototypes_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe PrototypesController do
-   let(:prototype) { create(:prototype) }
+   let!(:prototype) { create(:prototype, :with_sub_images) }
    let(:params) {{
      id: prototype.id,
      prototype: {
@@ -30,7 +30,7 @@ describe PrototypesController do
         get :show, id: prototype
         expect(assigns(:prototype)).to eq prototype
       end
-      
+
       it 'assigns likes associated with prototype to @likes' do
         get :show, id: prototype
         expect(assigns(:likes)).to eq prototype.likes
@@ -43,7 +43,20 @@ describe PrototypesController do
     end
 
     describe 'GET #edit' do
+      it 'assigns main_image to @main_image' do
+        get :edit, id: prototype
+        expect(assigns(:main_image)).to eq prototype.main_image
+      end
 
+      it 'assigns sub_images to @sub_images' do
+        get :edit, id: prototype
+        expect(assigns(:sub_images)).to eq prototype.captured_images.sub
+      end
+
+      it 'renders the :edit template' do
+        get :edit, id: prototype
+        expect(response).to render_template :edit
+      end
     end
 
     describe 'PATCH #update' do
